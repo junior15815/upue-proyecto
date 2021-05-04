0aabafcef611ef1497d1b0f7bd609beb529dddd2
junior15815 <>
2021-05-04 22:51:36 +0000

    Ejercicios 3.1 y 3.2

--- structure-condicional/EtsCondicional.py
+++ structure-condicional/EtsCondicional.py
@@ -26,5 +26,32 @@ def estCondicional02():
   montoP=cantidadX*75
  #Datos de salida
  print("El monto a pagar es:", montoP)
+def votoElecciones():
+  print ("Como saber si puedes votar por tu edad")
+  mensaje=""
+  edadP=int(input("ingrese la edad que tiene:"))
+  if edadP>=18:
+    mensaje="Usted tiene la edad necesaria para votar"
+  else:
+    mensaje="Usted no cumple con la edad minima para votar"
+  print(mensaje)
+
+def pagoSemanalBase40horas():
+  print("Pago semanal de trabajador")
+  sueldoPagarSem=0.0
+  #Datos de entrada
+  horasTra=int(input("Ingrese horas trabajadas a ala semana:"))
+  horasPago=int(input("Ingrese el pago por hora:"))
+  #Proceso
+  if horasTra>40:
+    sueldoPagarSem=40*horasPago+(horasTra-40)*2*horasPago
+  else:
+    sueldoPagarSem=horasTra*horasPago
+  #Datos de salida
+  print("El sueldo a pagar al trabajador es:",sueldoPagarSem)
+
+
 #estCondicional02()
-estCondicional02()
+#estCondicional02()
+#votoElecciones()
+pagoSemanalBase40horas()
