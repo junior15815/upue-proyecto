0fe0caf0f0900cd292790c7aaa94786486907dc0
[junior15815] <[juniorvilcapaza15@gmail.com]>
2021-05-15 23:28:15 -0500

    Eejemplo 1 etr.repetitive

--- structure-repetitive/EtrRepetitive.java
+++ structure-repetitive/EtrRepetitive.java
@@ -1,4 +1,10 @@
+
+import java.util.Scanner;
+/**
+ * EtrRepetitive
+ */
 public class EtrRepetitive {
+
     static Scanner teclado=new Scanner(System.in);
 
     public static void suma10NumerosMientras() {
@@ -14,36 +20,9 @@ public class EtrRepetitive {
         }
         System.out.println("La suma de los 10 numeros es:"+sumaNumeros);
     }
-    public static void suma10NumerosHacerMientras() {
-        //Daclarar Variables
-        int contador=1;
-        double numeros, sumaNumeros=0;
-        //Datos de entrada y Proceso
-        do{
-            System.out.println("Ingrese el valor en la posicion "+contador+":");
-            numeros=teclado.nextDouble();
-            sumaNumeros=sumaNumeros+numeros;
-            contador++;
-        }
-        while (contador<=10);
-        System.out.println("La suma de los 10 numeros es:"+sumaNumeros);
-    } 
-    public static void suma10NumerosPara() {
-        //Daclarar Variables        
-        double numeros, sumaNumeros=0;
-        //Datos de entrada y Proceso
-        for(int contador=1; contador<=10; contador++){
-            System.out.println("Ingrese el valor en la posicion "+contador+":");
-            numeros=teclado.nextDouble();
-            sumaNumeros=sumaNumeros+numeros;            
-        }
-        System.out.println("La suma de los 10 numeros es:"+sumaNumeros);
-    }        
 
-    public static void main(String[] args) {
-        System.out.println("Holas Mundo!!");
-        //suma10NumerosMientras();
-        //suma10NumerosHacerMientras();
-        suma10NumerosPara();
+    public static void main (String[] args) {
+        System.out.println("Hola mundo");
+        suma10NumerosMientras();
     }
 }
